8c5d3ef8501a74808dd30b6476df2feeb78ded40
Ben <applegateben03@gmail.com>
2025-10-19 10:01:13 -0600

    Refactored shader code to properly load data

--- src/engine/shader.c
+++ src/engine/shader.c
@@ -7,6 +7,9 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 
+static const struct shader blank_shader = {0};
+
+
 static const char* testVertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
@@ -14,7 +17,7 @@ static const char* testVertexShaderSource = "#version 330 core\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
-static const char* testFragmentShaderSource = 
+static const char* testFragmentShaderSource =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main(){\n"
@@ -24,13 +27,11 @@ static const char* testFragmentShaderSource =
 struct shader load_test_shader_program(){
     struct shader test_shader;
 
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &testVertexShaderSource, 0);
     glCompileShader(vertexShader);
 
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &testFragmentShaderSource, 0);
     glCompileShader(fragmentShader);
 
@@ -43,6 +44,7 @@ struct shader load_test_shader_program(){
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    return test_shader;
 }
 
 struct shader load_shader(const char* vertex_source, const char* fragment_source){
@@ -50,36 +52,34 @@ struct shader load_shader(const char* vertex_source, const char* fragment_source
     int vertexFD = open(vertex_source, O_RDONLY, 0);
     if(vertexFD == -1){
         fprintf(stderr, "Could not open the specified vertex shader");
-        return;
+        return blank_shader;
     }
     struct stat vertexStats;
     fstat(vertexFD, &vertexStats);
-    char* vertex_shader_source = mmap(0, vertexStats.st_size, PROT_READ, MAP_PRIVATE, vertexFD, 0);
-    printf("%s\n", vertex_shader_source);
+    const char* vertex_shader_source = mmap(nullptr, vertexStats.st_size, PROT_READ, MAP_PRIVATE, vertexFD, 0);
     //Compile vertex shader
     int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertex_shader_source, nullptr);
     glCompileShader(vertexShader);
-    printf("Compiled vertex shader\n");
 
     int success;
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
     if(!success){
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
         fprintf(stderr, "Failed to compile vertex shader: %s\n", infoLog);
     }
-    munmap(vertex_shader_source, vertexStats.st_size);
+    munmap((void*)vertex_shader_source, vertexStats.st_size);
     close(vertexFD);
 
     //Open and map fragment shader source
     int fragmentFD = open(fragment_source, O_RDONLY, 0);
     if(fragmentFD == -1){
         fprintf(stderr, "Could not open the specified vertex shader");
-        return;
+        return blank_shader;
     }
     fstat(vertexFD, &vertexStats);
-    char* fragment_shader_source = mmap(0, vertexStats.st_size, PROT_READ, MAP_PRIVATE, fragmentFD, 0);
+    const char* fragment_shader_source = mmap(nullptr, vertexStats.st_size, PROT_READ, MAP_PRIVATE, fragmentFD, 0);
 
     //Compile fragment shader
     int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -87,11 +87,11 @@ struct shader load_shader(const char* vertex_source, const char* fragment_source
     glCompileShader(fragmentShader);
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if(!success){
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
         fprintf(stderr, "Failed to compile fragment shader: %s\n", infoLog);
     }
 
-    munmap(fragment_shader_source, vertexStats.st_size);
+    munmap((void*) fragment_shader_source, vertexStats.st_size);
     close(fragmentFD);
 
     struct shader program;
@@ -101,7 +101,7 @@ struct shader load_shader(const char* vertex_source, const char* fragment_source
     glLinkProgram(program.programID);
     glGetProgramiv(program.programID, GL_LINK_STATUS, &success);
     if(!success){
-        glGetProgramInfoLog(program.programID, 512, NULL, infoLog);
+        glGetProgramInfoLog(program.programID, 512, nullptr, infoLog);
         fprintf(stderr, "Failed to link shader program: %s\n", infoLog);
     }
     glUseProgram(program.programID);
